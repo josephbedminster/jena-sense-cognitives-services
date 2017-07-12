cba908e493cbc1b610d91a878225183d7d000551
josephbedminster <bedmin_j@etna-alternance.net>
2017-07-12 20:33:43 +0200

    Ajout script sh

---
+++ raspberry-server/send_photo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -X GET http://13.69.10.180/photoSend
+photo_path=/home/pi/Documents/SDC-WIND/photo.jpg
+raspistill -n -w 1200 -h 900 -t 1 -o $photo_path
+sshpass -p "Ggz8#e97hdp0gz" scp $photo_path jena@52.166.147.209:/var/www/html/uploads/photo.jpg && echo "File sent" + $(date)
